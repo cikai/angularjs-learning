4479b7fab4e4906325a0e1d082411888fe6f0da7
ci_kai <cikai0923@gmail.com>
2016-03-01 16:00:35 +0800

    add sample

---
+++ js/main.js
@@ -0,0 +1,7 @@
+angular.module('todoList', []).controller("TaskCtrl", function($scope) {
+    $scope.task = "";
+    $scope.tasks = [];
+    $scope.add = function() {
+        $scope.tasks.push($scope.task);
+    }
+});
